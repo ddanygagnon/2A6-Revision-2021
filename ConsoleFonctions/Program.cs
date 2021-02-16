8ac65bdd101acf162095d767b9fbad0d28335a75
ddanygagnon <ddanygagnon@gmail.com>
2021-02-16 13:16:42 -0500

    FIN 6 Code d'un autre projet

--- ConsoleFonctions/Program.cs
+++ ConsoleFonctions/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using RévisionLib;
+
 namespace ConsoleFonctions
 {
     internal class Program
@@ -9,7 +11,19 @@ namespace ConsoleFonctions
             Console.Title = "*** Dany Gagnon ***";
             Console.WriteLine("Bonjour, je suis Dany Gagnon!");
             Console.WriteLine("Vive Git!");
+            AfficherAdditionAléatoire();
+
             _ = Console.ReadKey();
+        }
+
+        private static void AfficherAdditionAléatoire()
+        {
+            var random = new Random();
+            int n1 = random.Next(100);
+            int n2 = random.Next(100);
+
+            Console.WriteLine(
+                $"{n1} + {n2} = {Fonctions.Additionner(n1, n2)}");
         }
     }
 }
--- RévisionLib/Fonctions.cs
+++ RévisionLib/Fonctions.cs
@@ -2,5 +2,9 @@
 {
     public class Fonctions
     {
+        public static int Additionner(int n1, int n2)
+        {
+            return n1 + n2;
+        }
     }
 }
